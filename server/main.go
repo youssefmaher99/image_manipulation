fb4eb7d6936c92c3dc4de58ca2ee947f31164f4a
Youssef Maher <youssefmaher46@yahoo.com>
2022-10-26 16:44:20 +0200

    graceful shutdown cleaning dirs (for dev purposes)

--- server/main.go
+++ server/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
 	"server/handlers"
 	"server/router"
 	"server/util"
@@ -14,17 +18,43 @@ import (
 var MyQueue *util.Queue
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	MyQueue = util.CreateQueue()
+
 	go func(MyQueue *util.Queue) {
 		util.SpawnWorkers(MyQueue)
 	}(MyQueue)
 
+	// Inject global queue in package
+	handlers.MyQueue = MyQueue
+
+	// Graceful shutdown cleaning dirs
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	go func() {
+		// blocking
+		<-sigChan
+
+		// clean up
+		cleanDirs(map[string]string{"uploaded": "jpg", "filtered": "jpg", "archives": "gz"})
+	}()
+
 	r := router.CreateChiRouter(middleware.Logger)
 	router.LoadRoutes(r)
 
-	// Package injection
-	handlers.MyQueue = MyQueue
-
 	log.Println("Server is connected")
 	log.Fatal(http.ListenAndServe(":5000", r))
+}
+
+func cleanDirs(dirs map[string]string) {
+	for dir, ext := range dirs {
+		cmd := exec.Command("bash", "-c", fmt.Sprintf("rm %s/*.%s", dir, ext))
+		err := cmd.Run()
+		// if dirs are empty err != nil so i continue to check the other dirs
+		if err != nil {
+			continue
+		}
+	}
+	os.Exit(0)
 }
